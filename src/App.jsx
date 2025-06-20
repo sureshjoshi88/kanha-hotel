99df4a083548a45349e5334169a2dc3e18c24ce8
sureshjoshi88 <sj3795789@gmail.com>
2025-06-20 13:56:38 +0530

    make a hotel project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+  <>
+  <h1 className='text-2xl text-orange-400 font-medium '>jai shree ram</h1>
+  </>
+  )
+}
+
+export default App
