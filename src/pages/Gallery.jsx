8ab3fba4b45125b75c719642dc98184cdcf2d422
sureshjoshi88 <sj3795789@gmail.com>
2025-07-13 15:14:04 +0530

    update

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { hotelImages } from '../data/Gellery'
 const Gallery = () => {
-    console.log(hotelImages);
-    
+  console.log(hotelImages);
+
   return (
     <div>
       <div className='grid gap-4 md:grid-cols-3 mt-4  p-2 bg-gray-200 '>
-{hotelImages.map((item)=>(
-    <div key={item.id} className='overflow-hidden'>
-         <img className='h-60 object-cover w-full hover:scale-120' src={item.url} alt="" />
-    </div>
-))}
+        {hotelImages.map((item) => (
+          <div key={item.id} className='overflow-hidden'>
+            <img className='h-62 object-cover w-full hover:scale-120' src={item.url} alt="" />
+          </div>
+        ))}
       </div>
     </div>
   )
