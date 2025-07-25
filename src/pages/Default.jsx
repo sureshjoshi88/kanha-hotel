dd57a1405de1cf02d95eb4cd656271a17576d143
sureshjoshi88 <sj3795789@gmail.com>
2025-07-25 14:30:36 +0530

    styling default page

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -5,7 +5,7 @@ const Default = () => {
     <div className='flex justify-center items-center h-100 w-full'>
       <div>
         <p className='font-medium text-3xl '>Page is not found</p>
-      <NavLink className='font-medium text-xl' to='/'><button className='bg-blue-400 rounded p-1 text-white ps-2 pe-2  m-5 cursor-pointer'>Go to home page</button></NavLink>
+      <NavLink className='font-medium text-xl' to='/'><button className='bg-blue-600 rounded p-1 ps-2 pe-2 text-white ps-2 pe-2  m-5 cursor-pointer'>Go to home page</button></NavLink>
       </div>
     </div>
   )
