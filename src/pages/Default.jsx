e5179d9abd67331d5386db07fbec9397527d40c2
sureshjoshi88 <sj3795789@gmail.com>
2025-08-08 15:18:37 +0530

    add new text for default page

--- src/pages/Default.jsx
+++ src/pages/Default.jsx
@@ -5,6 +5,7 @@ const Default = () => {
     <div className='flex justify-center items-center h-100 w-full'>
       <div>
         <p className='font-medium text-3xl '>Page is not found</p>
+        <p>please back to home page</p>
       <NavLink className='font-medium text-xl' to='/'><button className='bg-blue-600 rounded p-1 ps-2 pe-2 text-white ps-2 pe-2  m-5 cursor-pointer'>Go to home page</button></NavLink>
       </div>
     </div>
